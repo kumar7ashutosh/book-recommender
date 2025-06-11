13693df12042b8448708008f9ec70982ed254fec
kumar7ashutosh <kumarashutoshbtech2023@gmail.com>
2025-06-12 05:10:20 +0530

    validation

--- books_recommender/pipeline/training_pipeline.py
+++ books_recommender/pipeline/training_pipeline.py
@@ -1,5 +1,5 @@
 from books_recommender.components.stage_00_data_ingestion import DataIngestion
-# from books_recommender.components.stage_01_data_validation import DataValidation
+from books_recommender.components.stage_01_data_validation import DataValidation
 # from books_recommender.components.stage_02_data_transformation import DataTransformation
 # from books_recommender.components.stage_03_model_trainer import ModelTrainer
 
@@ -7,7 +7,7 @@ from books_recommender.components.stage_00_data_ingestion import DataIngestion
 class TrainingPipeline:
     def __init__(self):
         self.data_ingestion = DataIngestion()
-        # self.data_validation = DataValidation()
+        self.data_validation = DataValidation()
         # self.data_transformation = DataTransformation()
         # self.model_trainer = ModelTrainer()
 
@@ -17,6 +17,6 @@ class TrainingPipeline:
         :return: none
         """
         self.data_ingestion.initiate_data_ingestion()
-        # self.data_validation.initiate_data_validation()
+        self.data_validation.initiate_data_validation()
         # self.data_transformation.initiate_data_transformation()
         # self.model_trainer.initiate_model_trainer() 
