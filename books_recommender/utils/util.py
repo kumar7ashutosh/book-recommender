ab1ca2d12b557b5b1a124305360961f75313dffd
kumar7ashutosh <kumarashutoshbtech2023@gmail.com>
2025-06-12 01:19:44 +0530

    utils

---
+++ books_recommender/utils/util.py
@@ -0,0 +1,9 @@
+import yaml,sys
+from books_recommender.exception.exception_handler import AppException
+
+def read_yaml_file(file_path:str)->dict:
+    try:
+        with open(file_path,'w') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise AppException(e,sys) from e
